e62fe58c41b5bb5d8f0f090d53648bc62b0e70f2
Kim Quibral <kimquibral2000@gmail.com>
2023-07-16 14:35:31 +0800

    isPending to isDelivered

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
-
 while true; do
     echo "Please choose an option:"
-    echo "1. BUILD APK RELEASE"
-    echo "2. PUSH TO GITHUB"
-    echo "3. PERFORM ALL OPTIONS (1-2)"
+    echo "1. PUSH TO GITHUB"
+    echo "2. BUILD APK RELEASE + PUSH TO GITHUB"
     echo "0. EXIT"
 
     read -p "Enter your choice: " choice
 
     if [ "$choice" = "1" ]; then
-        echo "=================================================="
-        echo "Running flutter build apk --release ..."
-        echo "=================================================="
-        flutter build apk --release
-
-    elif [ "$choice" = "2" ]; then
         read -p "Enter commit message: " commit_message
 
         echo "=================================================="
@@ -33,7 +24,7 @@ while true; do
         echo "=================================================="
         git push -u origin main
 
-    elif [ "$choice" = "3" ]; then
+    elif [ "$choice" = "2" ]; then
         echo "=================================================="
         echo "Running flutter build apk --release ..."
         echo "=================================================="
